2870bbd4f30704b41dda0899b68f03efff23c7da
shreyaadulla30 <shreyareddy.adulla@gmail.com>
2024-02-03 11:08:18 +0530

    commit

--- obscuredImages/obscure_db.py
+++ obscuredImages/obscure_db.py
@@ -3,7 +3,7 @@ import sqlite3
 import os
 
 
-conn = sqlite3.connect("garbled_db.db")
+conn = sqlite3.connect("obscure_db.db")
 cursor = conn.cursor()
 
 print('Database created')
@@ -19,11 +19,11 @@ cursor.execute('''
 conn.commit()
 print('Table Created')
 
-def insert_data(filename, original_text):
+def insert_data(filename, original_obscure):
     # Insert data into the table
-    cursor.execute('INSERT INTO garbled_table (filename, original_text) VALUES (?, ?)', (filename, original_text))
+    cursor.execute('INSERT INTO garbled_table (filename, original_obscure) VALUES (?, ?)', (filename, original_obscure))
     
-image_folder = 'C:/Users/91970/Desktop/major/Graphical-Passoword-Non-Human/garbledImages'
+image_folder = 'C:/Users/91970/Desktop/major/Graphical-Passoword-Non-Human/obscureImages'
 
 # Iterate through each file in the folder
 for filename in os.listdir(image_folder):
@@ -31,11 +31,11 @@ for filename in os.listdir(image_folder):
         # You may want to extract original_text from the file or provide a default value
         base_name, extension = os.path.splitext(filename)
         original_text = base_name
-        h = hashlib.new('sha512_256') 
-        h.update(base_name.encode())
-        original_text = h.hexdigest()
+        # h = hashlib.new('sha512_256') 
+        # h.update(base_name.encode())
+        # original_text = h.hexdigest()
         # Insert data into the authentication_info table
-        insert_data(filename, original_text)
+        insert_data(filename, original_obscure)
 
 print('Data filled successfully...')   
 conn.commit()
